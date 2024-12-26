b22b5af67fbdbfe291feb8a516269fec11c8bf06
Jack <jcm2022@icloud.com>
2024-12-26 02:06:25 -0500

    fixed base distribution

--- NICE.py
+++ NICE.py
@@ -10,12 +10,14 @@ class StandardLogisticDistribution(nn.Module):
     def __init__(self, D):
         super().__init__()
         base_distr = Uniform(low=torch.zeros(D), high=torch.ones(D))
-        transforms = [SigmoidTransform().inv, AffineTransform(loc=0, scale=1)]
+        transforms = [SigmoidTransform().inv, AffineTransform(torch.zeros(D), torch.ones(D))]
         self.logistic_distr = TransformedDistribution(base_distr, transforms)
 
     def log_pdf(self, z):
         self.logistic_distr.base_dist.low = self.logistic_distr.base_dist.low.to(z.device)
         self.logistic_distr.base_dist.high = self.logistic_distr.base_dist.high.to(z.device)
+        self.logistic_distr.transforms[1].loc = self.logistic_distr.transforms[1].loc.to(z.device)
+        self.logistic_distr.transforms[1].scale = self.logistic_distr.transforms[1].scale.to(z.device)
         return self.logistic_distr.log_prob(z).sum(dim=1)
 
     def sample(self):
