d35dc1025226d3eea853c73425b11067dfa4d05d
Jack <jcm2022@icloud.com>
2025-01-13 04:38:39 -0500

    RealNVP: in progress

--- RealNVP.py
+++ RealNVP.py
@@ -1,30 +1,80 @@
 import torch
 import torch.nn as nn
 from base_distributions import StandardNormalDistribution
+from neural_nets import ResNet
 
 class AffineCouplingLayer(nn.Module):
-    def __init__(self, D, split_half, resnet_hidden_channels=64):
+    def __init__(self, split_half, masking_scheme, resnet_hidden_channels=64):
         super().__init__()
-        self.D = D
-        self.d_1 = D // 2
-        self.d_2 = D - self.d_1
-        self.split_half = split_half
-        self.s = ResNet(in_channels=3, hidden_channels=resnet_hidden_channels)
+        self.split_half = split_half # 0 or 1
+        self.s = ResNet(in_channels=3, hidden_channels=resnet_hidden_channels, final_activation='scaled_tanh')
         self.t = ResNet(in_channels=3, hidden_channels=resnet_hidden_channels)
+        self.masking_scheme = masking_scheme # 'channelwise' or 'checkerboard'
 
     def checkerboard_mask(self, x):
-        # TODO: implement checkerboard style masking as seen in paper
-        pass
+        # Generates a checkerboard style mask b as seen in figure 3 & section 3.4 of paper
+        b = torch.ones_like(x)
+
+        # for each channel, zero out every other value of every other row
+        b[:, :, ::2, (self.split_half)::2] = 0
+
+        # for each channel, zero out every other value of every other row, both shifted by 1
+        b[:, :, 1::2, (1-self.split_half)::2] = 0
+
+        return b
+
     def channelwise_mask(self, x):
-        # TODO: implement channel-wise masking as seen in paper
-        pass
+        # Generates a channelwise style mask b as seen in figure 3 & section 3.4 of paper
+        b = torch.ones_like(x)
+
+        # zero out every other channel, starting based on the split half of this layer
+        b[:, (self.split_half)::2, :, :] = 0
+
+        return b
 
     def forward(self, x):
-        # TODO: mask, return out = s(masked_input) * (other_mask) + t(masked_input)  
-        pass
+        b = checkerboard_mask(x) if self.masking_scheme is 'checkerboard' else channelwise_mask(x)
+
+        # equation 9 of paper
+        y = (b * x) + (1 - b) * (x * torch.exp(self.s(b * x)) + self.t(b * x))
+        return y
+
+class RealNVP_Level(nn.Module):
+    def __init__(self, n_components, start_mask, resnet_hidden_channels=64):
+        super().__init__()
+
+        self.checkerboard_masking_layers = nn.Sequential(
+            AffineCouplingLayer(start_mask, 'checkerboard', resnet_hidden_channels),
+            AffineCouplingLayer(1-start_mask, 'checkerboard', resnet_hidden_channels),
+            AffineCouplingLayer(start_mask, 'checkerboard', resnet_hidden_channels),
+            )
+
+        self.channelwise_masking_layers = nn.Sequential(
+            AffineCouplingLayer(start_mask, 'channelwise', resnet_hidden_channels),
+            AffineCouplingLayer(1-start_mask, 'channelwise', resnet_hidden_channels),
+            AffineCouplingLayer(start_mask, 'channelwise', resnet_hidden_channels),
+            )
+
+    def squeeze(self, x):
+        # see section 3.6 of paper
+        n = x.shape[0]
+        c = x.shape[1]
+        h = x.shape[2]
+        w = x.shape[3]
+        assert h % 2 == w % 2 == 0
+
+        return x.view(n, 4*c, h/2, w/2)
+
+    def forward(self, x):
+        h = self.checkerboard_masking_layers(x)
+        h = self.squeeze(h)
+        h = channelwise_masking_layers(h)
+        return h
 
 class RealNVP(nn.Module):
-    super().__init__()
-    self.base_distr = StandardNormalDistribution() # needs some kind of dimension: How do 2d inputs get mapped to a standard normal?
+    def __init__(self):
+        super().__init__()
+        self.base_distr = StandardNormalDistribution() # needs some kind of dimension: How do 2d inputs get mapped to a standard normal?
+
 
 
--- neural_nets.py
+++ neural_nets.py
@@ -20,6 +20,7 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+# ResNets modeled after the torchvision implementation: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
 class ResNetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, padding=1 ):
         super().__init__()
@@ -31,7 +32,7 @@ class ResNetBlock(nn.Module):
         self.relu  = nn.ReLU()
 
     def forward(self, x):
-        # x should be NxCx32x32 for CIFAR10
+        # x should be NxCxHxW
         identity = x
 
         out = self.conv1(x)
@@ -46,17 +47,24 @@ class ResNetBlock(nn.Module):
         out = self.relu(out)
 
 class ResNet(nn.Module):
-    def __init__(self, in_channels, hidden_channels, n_blocks=8):
+    def __init__(self, in_channels, hidden_channels, n_blocks=8, final_activation='none'):
         super().__init__()
-        self.n_blocks= n_blocks
+        self.final_activation = final_activation
 
         self.blocks = nn.ModuleList([
             [ResNetBlock(in_channels, hidden_channels)] + # first block
-            [ResNetBlock(hidden_channels, hidden_channels) for _ in range(self.n_blocks - 2)] + # All blocks that aren't first or last are 'middle blocks'
+            [ResNetBlock(hidden_channels, hidden_channels) for _ in range(n_blocks - 2)] + # All blocks that aren't first or last are 'middle blocks'
             [ResNetBlock(hidden_channels, in_channels)] # last block
             ])
+
+        # TODO: learned scale
+        self.learned_scale = nn.Parameter(torch.tensor())
 
     def forward(self, x):
         for residual_block in self.blocks:
             x = residual_block(x)
+
+        if self.final_activation == 'scaled_tanh':
+            x = self.learned_scale * torch.tanh(x)
+
         return x
